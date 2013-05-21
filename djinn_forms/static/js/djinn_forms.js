99716a08911ab520ee62d37c190ea974df45cdfb
Duco Dokter <dokter@w20e.com>
2013-05-21 10:33:09 +0200

    hide modal

--- djinn_forms/static/js/djinn_forms.js
+++ djinn_forms/static/js/djinn_forms.js
@@ -14,5 +14,7 @@ djinn.forms.set_link = function(url, content_type, object_id, title, extra_args)
   var val = url + "::" + content_type + "::" + object_id;
 
   $("#id_" + extra_args['tgt']).val(val);
-  $("#" + extra_args['tgt'] + "_link").html(title || url);  
+  $("#" + extra_args['tgt'] + "_link").html(title || url);
+
+  $("#MyModal").modal("hide");
 };
