fa9b5c368abef08499279efd5595eba4b1204b77
Duco Dokter <dokter@w20e.com>
2013-07-08 20:20:40 +0200

    Stable release

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ requires = [
     ]
 
 setup(name='djinn_forms',
-      version="1.0.0-snapshot",
-      description='PythonUnited Intranet Forms',
+      version="1.0.0",
+      description='Djinn Intranet Forms',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: Freely Distributable",
