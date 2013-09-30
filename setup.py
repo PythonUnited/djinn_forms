dbbd81f0e4c01d5d39011a821738696d3dacec99
Duco Dokter <dokter@w20e.com>
2013-09-30 10:02:46 +0200

    release 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_forms',
-      version="1.0.0",
+      version="1.0.1",
       description='Djinn Intranet Forms',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
