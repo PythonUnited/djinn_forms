214ef6d8f6ab6e25ac47f4cf465a0314885f0077
Duco Dokter <dokter@w20e.com>
2014-12-05 11:59:10 +0100

    Move to django 1.6.x

--- djinn_forms/urls.py
+++ djinn_forms/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url, patterns
 from pgsearch.views import ModalSearchView
 from pgsearch.forms import PGSearchForm, DocumentFilterSearchForm
 from djinn_forms.views.fileupload import UploadView
