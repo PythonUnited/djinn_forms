7ddb5ae3194af05e0ebd81c28ad814f735b4978d
Martin Bouma <bouma@egoactive.com>
2015-08-31 13:51:39 +0200

    preparing version 1.3.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
     ]
 
 setup(name='djinn_forms',
-      version="1.3.1",
+      version="1.3.3",
       description='Djinn Intranet Forms',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
