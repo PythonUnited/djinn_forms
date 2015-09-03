a299905f04c2a6db3e73f096044080bb2cecac1f
Jaqueline Berghout <jaqueline@geekaboo.nl>
2015-09-03 09:58:49 +0200

    initial

---
+++ djinn_forms/static/js/djinn_forms_datetimedirect.js
@@ -0,0 +1,27 @@
+/**
+ * DateTimeDirect widget JS
+ */
+
+$(document).ready(function() {
+    $.fn.changeradiofunction = function(){
+        $("#id_publish_from_date").attr("disabled", false);
+        $("#id_publish_from_time").attr("disabled", false);
+    }
+
+    if ($("#id_publish_from_date").val() || $("#id_publish_from_time").val()) {
+        $("#notdirect").attr("checked", "checked");
+        $.fn.changeradiofunction();
+    }
+
+    $('#notdirect').change( function () {
+        $.fn.changeradiofunction()
+    });
+
+    $('#direct').change( function () {
+        $("#id_publish_from_date").attr("disabled", true);
+        $("#id_publish_from_date").attr("value", "");
+        $("#id_publish_from_time").attr("disabled", true);
+        $("#id_publish_from_time").attr("value", "");
+    });
+
+});
