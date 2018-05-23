878b2b4e931b2214f40767250933e47cbf72d5f1
Martin Bouma <bouma@egoactive.com>
2018-05-23 14:56:27 +0200

    -1

--- djinn_forms/views/relate.py
+++ djinn_forms/views/relate.py
@@ -56,9 +56,8 @@ class RelateSearch(View):
         if content_types:
             _filter[ct_search_field] = content_types
 
-
         parentusergroup = request.GET.get("parentusergroup", None)
-        if parentusergroup:
+        if parentusergroup and parentusergroup != '-1':
             _filter['parentusergroup'] = parentusergroup
 
         sqs = SearchQuerySet().filter(**_filter)
